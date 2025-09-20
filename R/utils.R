3bcbdc519ccdb8328866254da91189e3f5d5a341
Javier Alonso Carrillo Longoria <135353762+AlonsoLongoria23@users.noreply.github.com>
2025-09-20 15:26:43 -0700

    Update utils.R
    TxDb_Tthymallus

--- R/utils.R
+++ R/utils.R
@@ -11,12 +11,13 @@ TXDBS = c(
     'TxDb.Rnorvegicus.UCSC.rn4.ensGene',
     'TxDb.Rnorvegicus.UCSC.rn5.refGene',
     'TxDb.Rnorvegicus.UCSC.rn6.refGene',
-    'TxDb.Dpulex.NCBI.ASM2113471v1.knownGene')
+    'TxDb.Dpulex.NCBI.ASM2113471v1.knownGene',
+    'TxDb.Thymallus.ASM434828v1')
 
 # org.* family of packages
 ORGDBS = data.frame(
     genome = c('dm3','dm6','galGal5','hg19','hg38','mm9','mm10','rn4','rn5','rn6','Dpulex'),
-    org = c('Dm','Dm','Gg','Hs','Hs','Mm','Mm','Rn','Rn','Rn','Dpulex'),
+    org = c('Dm','Dm','Gg','Hs','Hs','Mm','Mm','Rn','Rn','Rn','Dpulex', 'Tthymallus'),
     stringsAsFactors = FALSE)
 
 HMMCELLLINES = c('Gm12878','H1hesc','Hepg2','Hmec','Hsmm','Huvec','K562','Nhek','Nhlf')
